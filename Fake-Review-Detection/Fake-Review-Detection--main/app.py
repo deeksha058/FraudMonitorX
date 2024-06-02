90441103b76726e719779c73059d27f6a7395de1
deeksha058 <84802675+deeksha058@users.noreply.github.com>
2024-06-02 12:07:21 +0530

    Add files via upload

---
+++ Fake-Review-Detection/Fake-Review-Detection--main/app.py
@@ -0,0 +1,54 @@
+from flask import Flask, jsonify, render_template, request
+import numpy as np
+import pandas as pd
+import sklearn as sk
+import pickle
+import os
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
+from sklearn.model_selection import train_test_split
+
+
+
+app = Flask(__name__)
+
+model_path = r'C:\Users\dell\Downloads\Fake-Review-Detection--main\Fake-Review-Detection--main\model.pkl'
+
+model=pickle.load(open(model_path,'rb'))
+
+@app.route('/')
+def root():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET','POST'])
+def predict():
+    
+    directory = r'C:\Users\dell\Downloads\Fake-Review-Detection--main\Fake-Review-Detection--main'
+    file_name = 'deceptive-opinion.csv'
+
+    # Construct the file path using os.path.join
+    file_path = os.path.join(directory, file_name)
+
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv(file_path)
+    # df = pd.read_csv('deceptive-opinion.csv')
+    df1 = df[['deceptive', 'text']]
+    df1.loc[df1['deceptive'] == 'deceptive', 'deceptive'] = 0
+    df1.loc[df1['deceptive'] == 'truthful', 'deceptive'] = 1
+    X = df1['text']
+    Y = np.asarray(df1['deceptive'], dtype = int)
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3,random_state=109)
+    cv = CountVectorizer()
+    x = cv.fit_transform(X_train)
+    y = cv.transform(X_test)
+    message = request.form.get('enteredinfo')
+    data = [message]
+    vect = cv.transform(data).toarray()
+    pred = model.predict(vect)
+
+    return render_template('result.html', prediction_text=pred)
+
+    
+if __name__ == '__main__':
+    app.run(debug=True)
